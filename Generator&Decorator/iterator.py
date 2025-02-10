e7cec4424d72fe8d390b533f99708599551180e8
Jotin Hirom <jotin24613@gmail.com>
2025-02-10 18:51:27 +0530

    Add files via upload

---
+++ Generator&Decorator/iterator.py
@@ -0,0 +1,7 @@
+myList = [ 1,2,3,4,5]
+myL = iter(myList)
+print(next(myL))
+print(next(myL))
+print(next(myL))
+print(next(myL))
+print(next(myL))
---
+++ List,String,Tuple&Dict/funcDef.py
@@ -0,0 +1,4 @@
+def new_func(a,b):
+    return a+b,a-b
+add,dif = new_func(3,4)
+print(f"Sum is {add} and Difference is {dif}")
